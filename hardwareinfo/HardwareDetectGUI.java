c59b9a73f61cb43b720492ba100d2959a6530590
Adam Malone <65831036+adamm0019@users.noreply.github.com>
2023-01-10 22:59:43 +0000

    removed comments

--- hardwareinfo/HardwareDetectGUI.java
+++ hardwareinfo/HardwareDetectGUI.java
@@ -31,7 +31,7 @@ import oshi.util.FormatUtil;
 
 /**
  *
- * @author adam-
+ * @author adamm0019
  */
 public class HardwareDetectGUI extends javax.swing.JFrame {
 
@@ -842,11 +842,6 @@ public class HardwareDetectGUI extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Windows".equals(info.getName())) {
